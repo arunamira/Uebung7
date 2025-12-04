3dc1609b299690442fcdb5ffc7f0287c3508416b
Aruna Anhuf <a.anhuf@mail.com>
2025-12-04 16:21:45 +0100

    h1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,17 @@
+package h1;
+
+public class H1_main { public static void main(String[] args) {
+	
+Zahl b = new Zahl(300);
+
+b.setEven();
+b.setPositive();
+b.setSmall();
+
+System.out.print(b.even + " " + b.positive + " " + b.small);
+
+
+	
+}
+
+}
